c4ac180b044f7faeb1deec6bd7a8f825201ec2dd
s4113887rmit <s4113887@rmit.edu.vn>
2026-08-05 13:02:31 +0700

    Add InsuranceCard entity class

--- claimshield/InsuranceCard.java
+++ claimshield/InsuranceCard.java
@@ -1,4 +1,52 @@
 package claimshield;
+import java.time.LocalDateTime;
+/**
+ * @author Nguyen Ngoc Quang Dang - S4113887
+ */
 
 public class InsuranceCard {
+    private String cardNumber;
+    private String cardHolderId;
+    private String policyOwnerId;
+    private LocalDateTime expirationDate;
+
+    public InsuranceCard(String cardNumber,String cardHolderId,String policyOwnerId,LocalDateTime expirationDate) {
+        this.cardNumber = cardNumber;
+        this.cardHolderId = cardHolderId;
+        this.policyOwnerId = policyOwnerId;
+        this.expirationDate = expirationDate;
+    }
+
+    public String getCardNumber() {
+        return cardNumber;
+    }
+    public String getCardHolderId() {
+        return cardHolderId;
+    }
+    public String getPolicyOwnerId() {
+        return policyOwnerId;
+    }
+    public LocalDateTime getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setCardHolderId(String cardHolderId) {
+        this.cardHolderId = cardHolderId;
+    }
+    public void setPolicyOwnerId(String policyOwnerId) {
+        this.policyOwnerId = policyOwnerId;
+    }
+    public void setExpirationDate(LocalDateTime expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    public String toFileString() {
+        return cardNumber + "," + cardHolderId + "," + policyOwnerId + "," + expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuranceCard{cardNumber=" + cardNumber + ", cardHolderId=" + cardHolderId + ", policyOwnerId=" + policyOwnerId + ", expirationDate=" + expirationDate + "}";
+    }
+
 }
